dc846ebe3c2ae93d58ad832ce9d035483f7fbbdf
Don <hello@donbos.co>
2020-03-29 20:42:45 +0800

    first 40 achievements, some without titles yet

--- app/data/achievements.js
+++ app/data/achievements.js
@@ -1,53 +1,441 @@
-
 achievements = [
   {
     id: 1,
-    title: "Pushup Hero",
-    description: "Do 10 pushups",
+    title: "",
+    description: "Created an account. Welcome!",
     progress: 0,
-    goal: 10,
-    points: 200,
-    completed: false,
+    goal: 1,
+    points: 30,
     completed: false,
     imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
   },
   {
     id: 2,
-    title: "Pushup Hero",
-    description: "Do 10 pushups",
+    title: "",
+    description: "Started your 1st day of social distancing.",
     progress: 0,
-    goal: 10,
-    points: 200,
+    goal: 1,
+    points: 30,
     completed: false,
     imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
   },
   {
     id: 3,
-    title: "Pushup Hero",
-    description: "Do 10 pushups",
+    title: "",
+    description: "No breaches today.",
     progress: 0,
-    goal: 10,
-    points: 200,
+    goal: 90,
+    points: 75,
     completed: false,
     imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
   },
   {
     id: 4,
-    title: "Pushup Hero",
-    description: "Do 10 pushups",
+    title: "",
+    description: "Completed your 1st day of staying at home.",
     progress: 0,
-    goal: 10,
-    points: 200,
+    goal: 1,
+    points: 100,
     completed: false,
     imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
   },
   {
     id: 5,
-    title: "Pushup Hero",
-    description: "Do 10 pushups",
+    title: "",
+    description: "Completed a 3 day streak of staying at home.",
+    progress: 0,
+    goal: 1,
+    points: 150,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 6,
+    title: "",
+    description: "Completed a 5 day streak of staying at home.",
+    progress: 0,
+    goal: 1,
+    points: 200,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 7,
+    title: "",
+    description: "Completed a 7 day streak of staying at home.",
+    progress: 0,
+    goal: 1,
+    points: 250,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 8,
+    title: "",
+    description: "Completed a 10 day streak of staying at home.",
+    progress: 0,
+    goal: 1,
+    points: 300,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 9,
+    title: "",
+    description: "Completed a 14 day streak of staying at home.",
+    progress: 0,
+    goal: 1,
+    points: 400,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 10,
+    title: "",
+    description: "Completed a 30 day streak of staying at home.",
+    progress: 0,
+    goal: 1,
+    points: 500,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 11,
+    title: "",
+    description: "Completed a 40 day streak of staying at home.",
+    progress: 0,
+    goal: 1,
+    points: 800,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 12,
+    title: "",
+    description: "Completed a 60 day streak of staying at home.",
+    progress: 0,
+    goal: 1,
+    points: 1000,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 13,
+    title: "Nice",
+    description: "Completed a 69 day streak of staying at home. Nice.",
+    progress: 0,
+    goal: 1,
+    points: 1300,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 14,
+    title: "",
+    description: "Completed a 90 day streak of staying at home.",
+    progress: 0,
+    goal: 1,
+    points: 2000,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 15,
+    title: "",
+    description: "Broke a streak.",
+    progress: 0,
+    goal: 1,
+    points: 50,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 16,
+    title: "A 'good reason'",
+    description: "Gave a reason for a breach.",
+    progress: 0,
+    goal: 1,
+    points: 100,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 17,
+    title: "",
+    description: "Gave excuses for 20 breaches.",
+    progress: 0,
+    goal: 20,
+    points: 200,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 18,
+    title: "Gain Train I",
+    description: "Completed 10 pushups in a day.",
+    progress: 0,
+    goal: 1,
+    points: 50,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 19,
+    title: "Gain Train II",
+    description: "Completed 50 pushups in a day.",
+    progress: 0,
+    goal: 1,
+    points: 300,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 20,
+    title: "Gain Train III",
+    description: "Completed 100 pushups in a day.",
+    progress: 0,
+    goal: 1,
+    points: 500,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 21,
+    title: "Ab City I",
+    description: "Completed 10 situps in a day.",
+    progress: 0,
+    goal: 1,
+    points: 50,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 22,
+    title: "Ab City II",
+    description: "Completed 50 situps in a day.",
+    progress: 0,
+    goal: 1,
+    points: 300,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 23,
+    title: "Ab City III",
+    description: "Completed 100 situps in a day.",
+    progress: 0,
+    goal: 1,
+    points: 500,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 24,
+    title: "Philanthropist",
+    description: "Donated a roll of toilet paper to a friend.",
+    progress: 0,
+    goal: 1,
+    points: 150,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 25,
+    title: "",
+    description: "Haven't shaved in a while.",
+    progress: 0,
+    goal: 1,
+    points: 100,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 26,
+    title: "",
+    description: "Ate the same thing 3 days in a row.",
+    progress: 0,
+    goal: 1,
+    points: 300,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 27,
+    title: "Gordon Ramsay",
+    description: "Cooked a meal at home.",
+    progress: 0,
+    goal: 1,
+    points: 200,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 28,
+    title: "It's Wednesday, my dudes",
+    description: "Happy Humpday",
+    progress: 0,
+    goal: 1,
+    points: 250,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 29,
+    title: "Zoom zoom",
+    description: "Held a video call hangout with friends.",
+    progress: 0,
+    goal: 1,
+    points: 500,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 30,
+    title: "Socially Responsible Butterfly",
+    description: "Said 'no' to a in-person hangout invite.",
+    progress: 0,
+    goal: 1,
+    points: 500,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 31,
+    title: "",
+    description: "Went out to buy groceries.",
+    progress: 0,
+    goal: 1,
+    points: 250,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 32,
+    title: "",
+    description: "Got dressed up.",
+    progress: 0,
+    goal: 1,
+    points: 350,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 33,
+    title: "'Work From Home' Professional" ,
+    description: "Stayed in your pyjamas all day.",
+    progress: 0,
+    goal: 1,
+    points: 250,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 34,
+    title: "" ,
+    description: "Cleaned your phone once",
+    progress: 0,
+    goal: 1,
+    points: 100,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 35,
+    title: "" ,
+    description: "Cleaned your phone 5 times",
+    progress: 0,
+    goal: 5,
+    points: 250,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 36,
+    title: "" ,
+    description: "Cleaned your phone 10 times",
     progress: 0,
     goal: 10,
-    points: 200,
+    points: 400,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 36,
+    title: "" ,
+    description: "Cleaned your phone 20 times",
+    progress: 0,
+    goal: 20,
+    points: 800,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 34,
+    title: "" ,
+    description: "Washed your hands once",
+    progress: 0,
+    goal: 1,
+    points: 100,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 35,
+    title: "" ,
+    description: "Washed your hands 5 times",
+    progress: 0,
+    goal: 5,
+    points: 250,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 36,
+    title: "" ,
+    description: "Washed your hands 10 times",
+    progress: 0,
+    goal: 10,
+    points: 400,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 37,
+    title: "" ,
+    description: "Washed your hands 20 times",
+    progress: 0,
+    goal: 20,
+    points: 800,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 38,
+    title: "" ,
+    description: "Stayed up all night.",
+    progress: 0,
+    goal: 1,
+    points: 300,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 39,
+    title: "" ,
+    description: "Went to bed early.",
+    progress: 0,
+    goal: 1,
+    points: 300,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 40,
+    title: "",
+    description: "Gave us a rating on the Play Store",
+    progress: 0,
+    goal: 20,
+    points: 800,
     completed: false,
     imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
   }
