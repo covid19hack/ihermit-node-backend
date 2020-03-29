b69640d72d9fa6893bb3580cbf9185f70edd7af5
Jeremy Hon <jehon11@gmail.com>
2020-03-29 15:47:10 +0900

    check in routes

--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 const express = require('express');
 const usersController = require('./controllers/api/v1/users');
 const achievementsController = require('./controllers/api/v1/achievements');
+const checkInsController = require('./controllers/api/v1/checkIns');
 const authHelper = require('./helpers/auth')
 
 const router = express.Router();
 const usersRouter = express.Router();
 const achievementsRouter = express.Router();
+const checkInsRouter = express.Router();
 
 router.get('/ping', (req, res) => {
   res.json({"message": "pong"});
@@ -13,6 +15,7 @@ router.get('/ping', (req, res) => {
 
 router.use('/api/v1/users', usersRouter);
 router.use('/api/v1/achievements', achievementsRouter);
+router.use('/api/v1/checkIns', checkInsRouter);
 
 // users routes
 usersRouter.post('/authenticate', usersController.authenticate);
@@ -21,7 +24,10 @@ usersRouter.get('/profile', authHelper.authenticate, usersController.getProfile)
 usersRouter.post('/upsertAchievement', authHelper.authenticate, usersController.upsertAchievement);
 usersRouter.post('/createCheckIn', authHelper.authenticate, usersController.createCheckIn)
 
-//achievements routes
+// achievements routes
 achievementsRouter.get('/', achievementsController.getAllAchievements);
+
+// checkin routes
+checkInsRouter.patch('/:id/updateIsHome', authHelper.authenticate, checkInsController.updateIsHome);
 
 module.exports = router
