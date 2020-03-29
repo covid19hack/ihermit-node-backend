49d08061de7a160a42f8773de504cddbdddc6478
ParikshitChavan <parikshitschavan@gmail.com>
2020-03-29 19:19:53 +0900

    Change achievements

---
+++ app/data/achievements.js
@@ -0,0 +1,56 @@
+
+achievements = [
+  {
+    id: 1,
+    title: "Pushup Hero",
+    description: "Do 10 pushups",
+    progress: 0,
+    goal: 10,
+    points: 200,
+    completed: false,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 2,
+    title: "Pushup Hero",
+    description: "Do 10 pushups",
+    progress: 0,
+    goal: 10,
+    points: 200,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 3,
+    title: "Pushup Hero",
+    description: "Do 10 pushups",
+    progress: 0,
+    goal: 10,
+    points: 200,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 4,
+    title: "Pushup Hero",
+    description: "Do 10 pushups",
+    progress: 0,
+    goal: 10,
+    points: 200,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  },
+  {
+    id: 5,
+    title: "Pushup Hero",
+    description: "Do 10 pushups",
+    progress: 0,
+    goal: 10,
+    points: 200,
+    completed: false,
+    imgUrl: "http://s3.aws.com/covid19hack/dsa123dsa"
+  }
+]
+
+module.exports = achievements;
