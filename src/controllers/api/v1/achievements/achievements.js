3dda7fd1eda8a679191021fd69fa02288c94699f
ParikshitChavan <parikshitschavan@gmail.com>
2020-03-29 15:54:40 +0900

    Fix bugs

--- src/controllers/api/v1/achievements/achievements.js
+++ src/controllers/api/v1/achievements/achievements.js
@@ -1,11 +1,12 @@
 const createError = require('http-errors');
+const commonHelper = require('../../../../helpers/common');
 
 // Models and helpers
 const Achievement = require('../../../../models/achievement');
 
 const getAllAchievements = async (req, res, next) => {
   try{
-    return res.json( { ...await Achievement.find({}) } );
+    return res.json( await Achievement.find({}) );
   } catch (err){
     next (err)
   }
